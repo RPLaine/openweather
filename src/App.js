2c2acb5e2d4a8226f520bcda8b90b7bb821a8f77
RPLaine <riku.laine@gmail.com>
2023-02-14 01:06:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from "react";
 /* 
 !!! --- APIKEY --- !!!
 */
-const apiKey = "INSERT YOUR APIKEY"; /* !!! --- INSERT APIKEY HERE! --- !!! */
+const apiKey = "ENTER YOUR APIKEY"; /* !!! --- INSERT APIKEY HERE! --- !!! */
 
 /* 
 GPS locations for the cities 
